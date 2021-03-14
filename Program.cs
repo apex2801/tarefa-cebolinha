e00b61866566b7ece308cab855ad5ebc0b4550f4
apex2801 <aapinheiro2005@gmail.com>
2021-03-14 20:10:24 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tarefa_cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("talefa cebolinha");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("chame meu amigos pala joga video game aqui em casa mas so tinha um contlole");
+            Console.WriteLine("ai fomo na casa de outlo amigo meu pala pega outlo contlole");
+        
+        
+        }
+    }
+}
